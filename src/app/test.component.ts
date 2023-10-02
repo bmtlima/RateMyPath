d129ee8fc58b2ebbcdbeb4e57e3bc6a443fa8dad
David Bian <db442@duke.edu>
2023-10-02 02:30:56 -0400

    Added AWS database integration with Node.js

---
+++ src/app/test.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
+
+@Component({
+  selector: 'app-data',
+  templateUrl: './data.component.html',
+  styleUrls: ['./data.component.css'],
+})
+export class DataComponent implements OnInit {
+  data: any[] = [];
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.dataService.getData().subscribe((response) => {
+      this.data = response;
+    });
+  }
+}
